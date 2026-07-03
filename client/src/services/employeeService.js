a69528b059a6d24895b62841f6f6daaa6649e5e9
creatorsiddev-cloud <creatorsid.dev@gmail.com>
2026-07-03 12:01:43 +0530

    API name fault

--- client/src/services/employeeService.js
+++ client/src/services/employeeService.js
@@ -3,26 +3,26 @@ import axios from "axios";
 const API_URL = `${import.meta.env.VITE_API_URL}/employees`;
 
 export const getEmployees = async () => {
-  const response = await axios.get(API);
+  const response = await axios.get(API_URL);
   return response.data;
 };
 
 export const getEmployeeById = async (id) => {
-  const response = await axios.get(`${API}/${id}`);
+  const response = await axios.get(`${API_URL}/${id}`);
   return response.data;
 };
 
 export const addEmployee = async (employee) => {
-  const response = await axios.post(API, employee);
+  const response = await axios.post(API_URL, employee);
   return response.data;
 };
 
 export const updateEmployee = async (id, employee) => {
-  const response = await axios.put(`${API}/${id}`, employee);
+  const response = await axios.put(`${API_URL}/${id}`, employee);
   return response.data;
 };
 
 export const deleteEmployee = async (id) => {
-  const response = await axios.delete(`${API}/${id}`);
+  const response = await axios.delete(`${API_URL}/${id}`);
   return response.data;
 };
